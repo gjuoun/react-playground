543070ab2d8b682597a679392e8e5b40f5be9375
Jun Guo <gjuoun@gmail.com>
2022-12-22 15:16:16 -0400

    play with commands

--- monaco-editor/src/App.tsx
+++ monaco-editor/src/App.tsx
@@ -2,20 +2,50 @@ import React, { useEffect } from "react";
 import * as monaco from "monaco-editor";
 
 export const App = () => {
+  const [text, setText] = React.useState("");
+  const editorRef = React.useRef<monaco.editor.IStandaloneCodeEditor>();
+
   useEffect(() => {
-    const editor = monaco.editor.create(document.getElementById("monaco")!, {
-      value: 'function x() {console.log("Hello world!");}',
-      language: "javascript",
+    editorRef.current = monaco.editor.create(
+      document.getElementById("monaco")!,
+      {
+        value: text,
+        language: "javascript",
+      }
+    );
+    editorRef.current.onKeyUp((e) => {
+      console.log(e);
+      const newText = editorRef.current?.getValue();
+      console.log(newText);
     });
+
+    editorRef.current.addAction({
+      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS],
+      id: "save",
+      label: "Save",
+      run: () => {
+        console.log("save");
+      },
+    });
+
     return () => {
-      editor.dispose();
+      editorRef.current?.dispose();
+      editorRef.current = undefined;
     };
   }, []);
 
   return (
     <div>
       hello
-      <div id="monaco" style={{ height: 300, border: "1px solid gray" }}></div>
+      <div
+        id="monaco"
+        style={{ height: 300, border: "1px solid gray" }}
+        onBlur={() => {
+          const newText = editorRef.current?.getValue();
+
+          console.log(newText);
+        }}
+      ></div>
     </div>
   );
 };

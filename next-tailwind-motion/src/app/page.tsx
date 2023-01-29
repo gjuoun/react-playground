07921766d3065434b45feee5e2b250b971d491e5
Jun Guo <gjuoun@gmail.com>
2023-01-28 22:02:09 -0400

    gradient button

--- next-tailwind-motion/src/app/page.tsx
+++ next-tailwind-motion/src/app/page.tsx
@@ -2,14 +2,24 @@
 
 import { LayoutGroup, motion } from "framer-motion";
 import { useState } from "react";
-import { NeonButton } from "./ui/Button";
+import { GradientButton } from "./ui/Button";
 
 export default function Home() {
   return (
     <div className="w-full h-screen flex flex-col justify-center items-center ">
-      <div className="w-[120px] ">
-        <NeonButton />
-        <NeonButton color="blue" />
+      <div className="flex flex-col gap-4">
+        <GradientButton color="blue" blurSize="small">
+          Cancel
+        </GradientButton>
+        <GradientButton loading color="blue" blurSize="medium">
+          Loading
+        </GradientButton>
+        <GradientButton color="red" blurSize="small">
+          Confirm
+        </GradientButton>
+        <GradientButton color="red" blurSize="medium">
+          Loading
+        </GradientButton>
       </div>
     </div>
   );
--- next-tailwind-motion/src/app/ui/Button.tsx
+++ next-tailwind-motion/src/app/ui/Button.tsx
@@ -7,30 +7,52 @@ const colors = {
   blue: "conic-blue",
 };
 
-export const NeonButton = ({
+const blurSizeMap = {
+  small: "[filter:blur(5px)]",
+  medium: "[filter:blur(10px)]",
+  large: "[filter:blur(20px)]",
+};
+
+export const GradientButton = ({
   className,
   color = "red",
+  loading = false,
+  children,
+  blurSize = "medium",
 }: PropsWithChildren<{
   className?: string;
   color?: keyof typeof colors;
+  loading?: unknown;
+  blurSize?: keyof typeof blurSizeMap;
 }>) => {
-  const classes = clsx([
+  const buttonClasses = clsx([
     colors[color],
     "gradient-button py-2 px-4",
-    "animate-increase-angle hover:[animation-duration:.5s]",
+    "animate-increase-angle ",
+    Boolean(loading) && "[animation-duration:.5s]",
     className,
   ]);
 
+  const blurClasses = clsx([
+    "absolute top-[calc(100%/10)] left-0 right-0 bottom-0 z-[-3] ",
+    colors[color],
+    Boolean(loading) && "[animation-duration:.5s]",
+    blurSize && blurSizeMap[blurSize],
+    "animate-increase-angle ",
+  ]);
+
   return (
-    <div>
-      <button
-        className={classes}
-        // style={{
-        //   animationDuration: "1s",
-        // }}
-      >
-        Label
-      </button>
-    </div>
+    <button className={buttonClasses}>
+      {children}
+
+      {blurSize && (
+        <span
+          style={{
+            transform: "scale(0.9)",
+          }}
+          className={blurClasses}
+        ></span>
+      )}
+    </button>
   );
 };
